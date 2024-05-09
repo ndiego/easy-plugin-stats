da45f8759f58948dd9ea0cba27388fcb70ededa7
Nick Diego <nickmdiego@gmail.com>
2024-05-08 21:29:43 -0500

    Fresh build.

--- build/blocks/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '00ef8779ce7013ec991a');
---
+++ build/blocks/plugin-stats/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1f8b94e7678db4cbcbe1');
