233df91f7bc22c963e60411f40d6545538d790d2
Nick Diego <nickmdiego@gmail.com>
2024-05-09 20:58:23 -0500

    Fix the link output on the front end.

--- easy-plugin-stats.php
+++ easy-plugin-stats.php
@@ -120,15 +120,24 @@ class Easy_Plugin_Stats {
 			$suffix_output = '<span class="wp-block-post-terms__suffix">' . $suffix . '</span>';
 		}
 
+		$field_data = '';
+
 		if ( 1 === count( $slugs ) ) {
 			$plugin_data = $this->get_remote_plugin_data( $slugs[0], $cache );
-		} else {
-			$plugin_data = null;
+			$field_data  = $this->field_output( $attributes, $plugin_data );
+		} else if ( count( $slugs ) > 1 ) {
+			$aggregate_data = array();
+
+			foreach ( $slugs as $slug ) {
+				$plugin_data      = $this->get_remote_plugin_data( $slug, $cache );
+				$aggregate_data[] = $this->field_output( $attributes, $plugin_data, false, false );
+			}
+
+			$field_data = number_format( array_sum( $aggregate_data ), $this->number_format['decimals'], $this->number_format['dec_point'], $this->number_format['thousands_sep'] );
 		}
 
-		//@TODO add aggregate output.
 		$output  = $prefix_output;
-		$output .= $this->field_output( $attributes, $plugin_data, true );
+		$output .= $field_data;
 		$output .= $suffix_output;
 		
 		$html  = '<div ' . $wrapper_attributes . '>';
@@ -161,7 +170,7 @@ class Easy_Plugin_Stats {
 		// Fetch the plugin data.
 		$plugin_data = $this->get_remote_plugin_data( $slug, $cache );
 
-		return $this->field_output( $source_args, $plugin_data );
+		return $this->field_output( $source_args, $plugin_data, true, false );
 	}
 
 	public function enqueue_editor_scripts() {
@@ -246,7 +255,6 @@ class Easy_Plugin_Stats {
 				'screenshots',
 				'changelog',
 				'faq',
-				'short_description',
 				'download_link',
 				'support_link',
 				'tags',
@@ -267,24 +275,24 @@ class Easy_Plugin_Stats {
 			$plugin_data = $this->get_remote_plugin_data( $atts['slug'], $atts['cache_time'] );
 
 			$output  = html_entity_decode( $atts['before'] );
-			$output .= $this->field_output( $atts, $plugin_data );
+			$output .= $this->field_output( $atts, $plugin_data, true, false );
 			$output .= html_entity_decode( $atts['after'] );
 	
 			return $output;
 		
 		} else if ( $atts['type'] == 'aggregate' ) {
-			$data          = array();
+			$field_data    = array();
 			$cleaned_slugs = preg_replace( '/[^\w\-\s]/', ' ', $atts['slug'] ); // remove all characters that are not allowed
 			$cleaned_slugs = preg_replace( '/\s\s+/', ' ', $cleaned_slugs ); // trim all excess whitepace
 			$slugs         = explode( ' ', $cleaned_slugs );
 	
 			foreach ( $slugs as $slug ) {
-				$plugin_data = $this->get_remote_plugin_data( $slug, $atts['cache_time'] );
-				$data[]      = $this->field_output( $atts, $plugin_data );
+				$plugin_data  = $this->get_remote_plugin_data( $slug, $atts['cache_time'] );
+				$field_data[] = $this->field_output( $atts, $plugin_data, false, false );
 			}
 	
 			$output  = html_entity_decode( $atts['before'] );
-			$output .= number_format( array_sum( $data ), $this->number_format['decimals'], $this->number_format['dec_point'], $this->number_format['thousands_sep'] );
+			$output .= number_format( array_sum( $field_data ), $this->number_format['decimals'], $this->number_format['dec_point'], $this->number_format['thousands_sep'] );
 			$output .= html_entity_decode( $atts['after'] );
 	
 			return $output;
@@ -299,29 +307,31 @@ class Easy_Plugin_Stats {
 	 * @param array $atts         An array shortcode attributes
 	 * @param array $plugin_data  An array of all retrived plugin data from wp.org
 	 */
-	public function field_output( $atts, $plugin_data, $single = true ) {
+	public function field_output( $atts, $plugin_data, $single = true, $block = true ) {
 
-		if ( ! $plugin_data || ! isset( $plugin_data[ 'slug' ] ) ) {
+		if ( ! ( $plugin_data['slug'] ?? null ) ) {
 			return null;
 		}
 
-		$is_single = ( $single || $atts['type'] == 'single' );
-		$slug      = $plugin_data[ 'slug' ];
-		$rating    = isset( $plugin_data[ 'rating' ] ) ? $plugin_data[ 'rating' ] : '';
-		$sections  = isset( $plugin_data[ 'sections' ] ) ? (array) $plugin_data[ 'sections' ] : array();
+		$is_single = $single || ( isset( $atts['type'] ) && $atts['type'] === 'single' );
+		$slug      = $plugin_data['slug'];
+		$rating    = $plugin_data['rating'] ?? '';
+		$sections  = (array) ( $plugin_data['sections'] ?? [] );
 	
 		// Generate the shortcode output, some fields need special handling
 		switch ( $atts['field'] ) {
 			case 'active_installs':
-				$output = $is_single ? number_format( $plugin_data[ 'active_installs' ], $this->number_format['decimals'], $this->number_format['dec_point'], $this->number_format['thousands_sep'] ) : $plugin_data[ 'active_installs' ];
-				break;
 			case 'downloaded':
-				$output = $is_single ? number_format( $plugin_data[ 'downloaded' ], $this->number_format['decimals'], $this->number_format['dec_point'], $this->number_format['thousands_sep'] ) : $plugin_data[ 'downloaded' ];
+				$output = isset( $plugin_data[ $atts['field'] ] )
+					? $is_single
+						? number_format( $plugin_data[ $atts['field'] ], $this->number_format['decimals'], $this->number_format['dec_point'], $this->number_format['thousands_sep'] )
+						: $plugin_data[ $atts['field'] ]
+					: '';
 				break;
 			case 'contributors':
 				$contributors = (array) $plugin_data[ 'contributors' ];
-		
-				if ( ! empty ( $contributors ) ) {
+				$output       = '';
+				if ( ! empty( $contributors ) ) {
 					foreach ( $contributors as $contributor => $link ) {
 						$output[] = '<a href="' . esc_url( $link ) . '" target="_blank">' . esc_attr( $contributor ) . '</a>';
 					}
@@ -329,45 +339,26 @@ class Easy_Plugin_Stats {
 				}
 				break;
 			case 'five_rating':
-				if ( ! empty ( $rating ) ) {
-					$output = ( $rating / 100 ) * 5;
-				}
+				$output = ! empty( $rating ) ? ( $rating / 100 ) * 5 : '';
 				break;
 			case 'star_rating':
-
 				if ( ! empty( $rating ) ) {
 					$five_rating = ( $rating / 100 ) * 5;
-					$output      = '<span class="star-rating-container" title="' . $five_rating . " " . __( 'out of 5 stars', 'easy-plugin-stats' ) . '">';
-					$stars       = array_fill( 0, 5, 0 );
+					$output      = '<span class="star-rating-container" title="' . $five_rating . ' ' . __( 'out of 5 stars', 'easy-plugin-stats' ) . '">';
 					$filledStars = floor( $five_rating / 1 );
 					$halfStar    = round( $five_rating - $filledStars, 1 );
-				
-					for ( $i = 0; $i < $filledStars; $i++ ) {
-						$stars[ $i ] = 1;
-					}
-				
-					if ( $halfStar >= 0.5 ) {
-						$stars[ $filledStars ] = 5;
-					}
-
 					$star_empty  = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path fill-rule="evenodd" d="M9.706 8.646a.25.25 0 01-.188.137l-4.626.672a.25.25 0 00-.139.427l3.348 3.262a.25.25 0 01.072.222l-.79 4.607a.25.25 0 00.362.264l4.138-2.176a.25.25 0 01.233 0l4.137 2.175a.25.25 0 00.363-.263l-.79-4.607a.25.25 0 01.072-.222l3.347-3.262a.25.25 0 00-.139-.427l-4.626-.672a.25.25 0 01-.188-.137l-2.069-4.192a.25.25 0 00-.448 0L9.706 8.646zM12 7.39l-.948 1.921a1.75 1.75 0 01-1.317.957l-2.12.308 1.534 1.495c.412.402.6.982.503 1.55l-.362 2.11 1.896-.997a1.75 1.75 0 011.629 0l1.895.997-.362-2.11a1.75 1.75 0 01.504-1.55l1.533-1.495-2.12-.308a1.75 1.75 0 01-1.317-.957L12 7.39z" clip-rule="evenodd"></path></svg>';
-					$star_filled = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M11.776 4.454a.25.25 0 01.448 0l2.069 4.192a.25.25 0 00.188.137l4.626.672a.25.25 0 01.139.426l-3.348 3.263a.25.25 0 00-.072.222l.79 4.607a.25.25 0 01-.362.263l-4.138-2.175a.25.25 0 00-.232 0l-4.138 2.175a.25.25 0 01-.363-.263l.79-4.607a.25.25 0 00-.071-.222L4.754 9.881a.25.25 0 01.139-.426l4.626-.672a.25.25 0 00.188-.137l2.069-4.192z"></path></svg>';
-					$star_half   = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M9.518 8.783a.25.25 0 00.188-.137l2.069-4.192a.25.25 0 01.448 0l2.07 4.192a.25.25 0 00.187.137l4.626.672a.25.25 0 01.139.427l-3.347 3.262a.25.25 0 00-.072.222l.79 4.607a.25.25 0 01-.363.264l-4.137-2.176a.25.25 0 00-.233 0l-4.138 2.175a.25.25 0 01-.362-.263l.79-4.607a.25.25 0 00-.072-.222L4.753 9.882a.25.25 0 01.14-.427l4.625-.672zM12 14.533c.28 0 .559.067.814.2l1.895.997-.362-2.11a1.75 1.75 0 01.504-1.55l1.533-1.495-2.12-.308a1.75 1.75 0 01-1.317-.957L12 7.39v7.143z"></path></svg>';
-				
-					foreach ( $stars as $star ) {
-						switch ( $star ) {
-							case 0:
-								$output .= $star_empty;
-								break;
-							case 5:
-								$output .= $star_half;
-								break;
-							case 1:
-								$output .= $star_filled;
-								break;
+					$star_filled = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M11.776 4.454a.25.25 0 01.448 0l2.069 4.192a.25.25 0 00.188.137l4.626.672a.25.25 0 01.139.426l-3.348 3.263a.25.25 0 00-.072.222l.79 4.607a.25.25 0 01-.362.263l-4.138-2.175a.25.25 0 00-.232 0l-4.138 2.175a.25.25 0 01-.363-.263l.79-4.607a.25.25 0 00-.072-.222l-3.348-3.263a.25.25 0 01.139-.426l4.626-.672a.25.25 0 00.188-.137l2.069-4.192z"></path></svg>';
+					$star_half   = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M9.518 8.783a.25.25 0 00.188-.137l2.069-4.192a.25.25 0 01.448 0l2.07 4.192a.25.25 0 00.187.137l4.626.672a.25.25 0 01.139.427l-3.347 3.262a.25.25 0 00-.072.222l.79 4.607a.25.25 0 01-.363.264l-4.137-2.176a.25.25 0 00-.233 0l-4.138 2.175a.25.25 0 01-.362-.263l.79-4.607a.25.25 0 00-.072-.222l-3.348-3.263a.25.25 0 01.139-.426l4.625-.672zM12 14.533c.28 0 .559.067.814.2l1.895.997-.362-2.11a1.75 1.75 0 01.504-1.55l1.533-1.495-2.12-.308a1.75 1.75 0 01-1.317-.957L12 7.39v7.143z"></path></svg>';
+					foreach ( range( 0, 4 ) as $i ) {
+						if ( $i < $filledStars ) {
+							$output .= $star_filled;
+						} elseif ( $i === $filledStars && $halfStar >= 0.5 ) {
+							$output .= $star_half;
+						} else {
+							$output .= $star_empty;
 						}
 					}
-				
 					$output .= '</span>';
 				}
 				break;
@@ -375,41 +366,68 @@ class Easy_Plugin_Stats {
 				$date   = date_create( $plugin_data['last_updated'] );
 				$output = date_format( $date, $date_format );
 				break;
+			case 'tags':
+				$tags = (array) $plugin_data['tags'];
+				$output = ! empty( $tags ) ? implode( ', ', $tags ) : '';
+				break;
 			case 'description':
-				$output = $sections['description'];
-				break;
 			case 'installation':
-				$output = $sections['installation'];
-				break;
 			case 'screenshots':
-				$output = $sections['screenshots'];
-				break;
 			case 'changelog':
-				$output = $sections['changelog'];
-				break;
 			case 'faq':
-				$output = $sections['faq'];
+				$output = $sections[ $atts['field'] ] ?? '';
+				break;
+			// @TODO the following needs refactoring.
+			case 'homepage_link':
+				$link   = 'https://wordpress.org/plugins/' . $slug;
+				$output = ! $block ? $link : $this->formatLink( $atts, $link, __( 'Plugin Homepage', 'easy-plugin-stats' ) );
+				break;
+			case 'download_link':
+				$link   = $plugin_data[ $atts['field'] ] ?? '';
+				$output = ( $block && $link ) ? $this->formatLink( $atts, $link, __( 'Download', 'easy-plugin-stats' ) ) : $link;
 				break;
 			case 'live_preview_link':
-				$output = 'https://playground.wordpress.net/?plugin=' . $slug . '&blueprint-url=https://wordpress.org/plugins/wp-json/plugins/v1/plugin/' . $slug . '/blueprint.json';
+				$link   = 'https://playground.wordpress.net/?plugin=' . $slug . '&blueprint-url=https://wordpress.org/plugins/wp-json/plugins/v1/plugin/' . $slug . '/blueprint.json';
+				$output = ! $block ? $link : $this->formatLink( $atts, $link, __( 'Live Preview', 'easy-plugin-stats' ) );
 				break;
 			case 'support_link':
-				$output = 'https://wordpress.org/support/plugin/' . $slug;
+				$link   = 'https://wordpress.org/support/plugin/' . $slug;
+				$output = ! $block ? $link : $this->formatLink( $atts, $link, __( 'Support', 'easy-plugin-stats' ) );
 				break;
 			case 'reviews_link':
-				$output = 'https://wordpress.org/support/plugin/' . $slug . '/reviews';
+				$link      = 'https://wordpress.org/support/plugin/' . $slug . '/reviews';
+				$output = ! $block ? $link : $this->formatLink( $atts, $link, __( 'Reviews', 'easy-plugin-stats' ) );
 				break;
-			case 'tags':
-				$tags = (array) $plugin_data[ 'tags' ];
-
-				if ( ! empty( $tags ) ) {
-					$output = implode( ', ', $tags );
-				}
+			case 'author_profile':
+				$link   = $plugin_data[ $atts['field'] ] ?? '';
+				$output = ( $block && $link ) ? $this->formatLink( $atts, $link, __( 'Author Profile', 'easy-plugin-stats' ) ) : $link;
+				break;
+			case 'donate_link':
+				$link   = $plugin_data[ $atts['field'] ] ?? '';
+				$output = ( $block && $link ) ? $this->formatLink( $atts, $link, __( 'Donate', 'easy-plugin-stats' ) ) : $link;
 				break;
 			default:
-				$output = $plugin_data[ $atts['field'] ];
+				$output = $plugin_data[ $atts['field'] ] ?? '';
 		}
 		
+		return $output;
+	}
+
+	/**
+	 * Formats a hyperlink with provided attributes.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param array  $atts         Array of attributes for the hyperlink.
+	 * @param string $link         The URL of the hyperlink.
+	 * @param string $text_default The default text for the hyperlink if not provided in the attributes.
+	 * @return string              The formatted hyperlink HTML.
+	 */
+	public function formatLink( $atts, $link, $text_default ) {
+		$output  = '<a href="' . $link . '"';
+		$output .= $atts['linkTarget'] ? ' target="_blank">' : '>';
+		$output .= $atts['linkText'] ? $atts['linkText'] : $text_default;
+		$output .= '</a>';
 		return $output;
 	}
 
