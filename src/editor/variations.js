fb70dd74fdcaeb36cc6e280092f941260f17375f
Nick Diego <nickmdiego@gmail.com>
2024-06-09 21:45:06 -0500

    Add example, update default metadata, and change name.

--- src/editor/variations.js
+++ src/editor/variations.js
@@ -8,19 +8,25 @@ import { plugins } from '@wordpress/icons';
 registerBlockVariation( 'core/button', {
 	name: 'easy-plugin-stats/button',
 	icon: plugins,
-	title: __( 'Plugin Link', 'easy-plugin-stats' ),
+	title: __( 'Plugin Button', 'easy-plugin-stats' ),
 	description: __(
-		'Display a button that links to resources associated with plugins hosted on WordPress.org.',
+		'Prompt visitors to take action with a button-style link. This button links to resources associated with a plugin hosted on WordPress.org.',
 		'easy-plugin-stats'
 	),
-	scope: [ 'inserter', 'transform' ],
+	scope: [ 'inserter' ],
+	example: {
+		attributes: {
+			className: 'is-style-fill',
+			text: __( 'Download', 'easy-plugin-stats' ),
+		},
+	},
 	attributes: {
 		metadata: {
 			bindings: {
 				url: {
 					source: 'easy-plugin-stats/button',
 					args: {
-						field: 'download',
+						field: 'download_link',
 						slug: '',
 					},
 				},
